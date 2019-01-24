711e990351c3b2bf1c202828232ffe7409766e6c
Gavin Morris <gavin.morris@health.telstra.com>
2019-01-24 08:37:21 +0800

    add target process name to dump file name

--- dump.bat
+++ dump.bat
@@ -1,10 +1,11 @@
+@echo off
+
 rem %1 = process name
 
-@echo off
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set ldt=%ldt:~0,4%%ldt:~4,2%%ldt:~6,2%-%ldt:~8,2%%ldt:~10,2%
 
-set base=%cd%\%computername%-%ldt%
+set base=%cd%\%1-%computername%-%ldt%
 
 set dmp=%base%.dmp
 set mmp=%base%.mmp
@@ -12,11 +13,11 @@ set gcdmp=%base%.gcdump
 set zip=%base%.zip
 set log=%base%.log
 
-%cd%\procdump.exe -ma -a -r %1 -w %dmp%
+%cd%\procdump.exe -accepteula -ma -a -r %1 -w %dmp%
 
-%cd%\vmmap.exe -p %1 %mmp%
+%cd%\vmmap.exe -accepteula -p %1 %mmp%
 
-%cd%\PerfView.exe HeapSnapshotFromProcessDump %dmp% %gcdmp% -logfile:%log%
+%cd%\PerfView.exe -accepteula HeapSnapshotFromProcessDump %dmp% %gcdmp% -logfile:%log%
 
 rem -- zip the report files
 rem zip.exe -1j %zip% %gcdmp% %mmp% %log%
